3faf1bd26324f9ddc30ec1fb6cb093fd2c8c1206
David Eccles (gringer) <github@gringer.org>
2025-02-06 09:25:52 +1300

    Remove example HelloView plugin

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
 import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
 import { version } from '../package.json'
 
-import {
-  ReactComponent as HelloViewReactComponent,
-  stateModel as helloViewStateModel,
-} from './HelloView'
-
 import LinearManhattanDisplayF from './LinearManhattanDisplay'
 import LinearManhattanRendererF from './LinearManhattanRenderer'
 
@@ -17,25 +12,10 @@ export default class RepaverPlugin extends Plugin {
   version = version
 
   install(pluginManager: PluginManager) {
-    pluginManager.addViewType(() => {
-      return new ViewType({
-        name: 'HelloView',
-        stateModel: helloViewStateModel,
-        ReactComponent: HelloViewReactComponent,
-      })
-    })
     LinearManhattanDisplayF(pluginManager)
     LinearManhattanRendererF(pluginManager)
   }
 
   configure(pluginManager: PluginManager) {
-    if (isAbstractMenuManager(pluginManager.rootModel)) {
-      pluginManager.rootModel.appendToMenu('Add', {
-        label: 'Hello View',
-        onClick: (session: AbstractSessionModel) => {
-          session.addView('HelloView', {})
-        },
-      })
-    }
   }
 }
