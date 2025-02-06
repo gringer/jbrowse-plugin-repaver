37f9a755fe44377b52508c1f814ff44991ff4075
David Eccles (gringer) <github@gringer.org>
2025-02-07 00:17:56 +1300

    More package updates

--- src/RepeatDisplay/RepeatDisplayReactComponent.tsx
+++ src/RepeatDisplay/RepeatDisplayReactComponent.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BaseLinearDisplayComponent } from '@jbrowse/plugin-linear-genome-view'
 
 export default function RepeatDisplayReactComponent (props: any) {
--- src/RepeatDisplay/index.ts
+++ src/RepeatDisplay/index.ts
@@ -1,4 +1,5 @@
 import { lazy } from 'react'
+import React from 'react'
 
 import { DisplayType } from '@jbrowse/core/pluggableElementTypes'
 
